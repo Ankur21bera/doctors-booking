76d6817b6859d9ae5b3e21ef723cb9d20b653d2a
Ankur21bera <159689731+Ankur21bera@users.noreply.github.com>
2025-06-04 22:44:08 +0530

    Update Navbar.jsx

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -48,7 +48,12 @@ const Navbar = () => {
                 <div className='absolute right-0 mt-2 bg-stone-100 text-base font-medium text-gray-700 rounded shadow-md p-4 flex flex-col gap-2 z-30'>
                   <p onClick={() => { navigate('my-profile'); setShowDropdown(false); }} className='hover:text-black cursor-pointer'>My Profile</p>
                   <p onClick={() => { navigate('my-appointments'); setShowDropdown(false); }} className='hover:text-black cursor-pointer'>My Appointment</p>
-                  <p onClick={() => {logout} className='hover:text-black cursor-pointer'>Logout</p>
+                 <p onClick={() => {
+  logout();
+  setShowDropdown(false);
+  navigate('/');
+}} className='hover:text-black cursor-pointer'>Logout</p>
+
                 </div>
               )}
             </div>
